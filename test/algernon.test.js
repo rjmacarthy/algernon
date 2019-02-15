ad839762f091fd8b732b7582794c75dcedb6b9bc
rjmacarthy <richardmacarthy@hotmail.com>
2019-02-15 17:03:25 +0000

    More tests and fix base eth test

--- test/algernon.test.js
+++ test/algernon.test.js
@@ -6,14 +6,20 @@ var Algernon = require('../lib/algernon')
 describe('integration', () => {
   it('can parse an abi and create folder and interfaces', () => {
     new Algernon({
-      path: './test-abis'
+      path: path.join(__dirname, '/test-abis')
     }).parse()
     assert.strictEqual(existsSync('../algernon'), true)
   })
 
-  it('parses the interface correctly', () => {
+  it('It writes the eth base interface correctly', () => {
     const file = readFileSync(path.join(__dirname, '../algernon/EthBase.ts'), 'utf-8').trim().replace(/[\n\r]+/g, '').replace(/\s{2,10}/g, ' ')
     let expected = `export interface EthBase { call: (options?: any, callback?: Function) => Promise<any>, send: (options: any, callback?: Function) => Promise<any>, estimateGas: (options?: any, callback?: Function) => Promise<any>, encodeABI: () => Promise<any> }`
+    assert.strictEqual(file, expected)
+  })
+
+  it('It writes the base interface correctly', () => {
+    const file = readFileSync(path.join(__dirname, '../algernon/Base.ts'), 'utf-8').trim().replace(/[\n\r]+/g, '').replace(/\s{2,10}/g, ' ')
+    let expected = `import { EthBase } from './EthBase'; export interface Base extends EthBase { counter : () => EthBase;}`	
     assert.strictEqual(file, expected)
   })
 })
