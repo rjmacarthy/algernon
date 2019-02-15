1bc9381c95942b5f841024feafc53157d6823836
rjmacarthy <richardmacarthy@hotmail.com>
2019-02-15 17:27:40 +0000

    Extend eth base interface

--- test/algernon.test.js
+++ test/algernon.test.js
@@ -10,13 +10,7 @@ describe('Integration tests', () => {
     }).parse()
     assert.strictEqual(existsSync('../algernon'), true)
   })
-
-  it('It writes the eth base interface correctly', () => {
-    const file = readFileSync(path.join(__dirname, '../algernon/EthBase.ts'), 'utf-8').trim().replace(/[\n\r]+/g, '').replace(/\s{2,10}/g, ' ')
-    let expected = `export interface EthBase { call: (options?: any, callback?: Function) => Promise<any>, send: (options: any, callback?: Function) => Promise<any>, estimateGas: (options?: any, callback?: Function) => Promise<any>, encodeABI: () => Promise<any> }`
-    assert.strictEqual(file, expected)
-  })
-
+  
   it('It writes the base interface correctly', () => {
     const file = readFileSync(path.join(__dirname, '../algernon/Base.ts'), 'utf-8').trim().replace(/[\n\r]+/g, '').replace(/\s{2,10}/g, ' ')
     let expected = `import { EthBase } from './EthBase'; export interface Base extends EthBase { counter : () => EthBase;}`
